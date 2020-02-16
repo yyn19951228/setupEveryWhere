ca750337d09c8631be4d986ec4db511afb46d64a
Ubuntu <ubuntu@ip-172-31-47-159.ec2.internal>
2020-02-16 04:42:12 +0000

    uodate some scripts

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+# add check for vim version and update vim if needed
+
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # download node
 curl -sL install-node.now.sh/lts | bash
 
